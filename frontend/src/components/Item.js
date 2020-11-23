c0b8554a5389986a0905b80adf84b1a0379c207a
salwa a <SalwaBaqer>
2020-11-24 01:53:18 +0300

    checked

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,17 +1,47 @@
 //style
-import { ItemWrapper } from "../styles";
+import { ItemWrapper, ItemWrapperChecked } from "../styles";
 
 //Libraries
 import { observer } from "mobx-react";
 
+//state
+import { useState } from "react";
+
 const Item = ({ name, priority, status }) => {
-  return (
-    <ItemWrapper>
-      <p> {name}, </p>
-      <p> priority: {priority}</p>
-      <p>, current status: {status}</p>
-    </ItemWrapper>
-  );
+  const handleChange = (event) => {
+    if (box === "on") setBox("");
+    else setBox("on");
+  };
+  const [box, setBox] = useState("");
+  if (box === "on") {
+    return (
+      <>
+        <input type="checkbox" id="checked" onChange={handleChange} />
+        <label for="checked">
+          <ItemWrapperChecked>
+            <p> {name}, </p>
+            <p> priority: {priority}</p>
+            <p>, current status: {status}</p>
+          </ItemWrapperChecked>
+        </label>
+        <br></br>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <input type="checkbox" id="unchecked" onChange={handleChange} />
+        <label for="unchecked">
+          <ItemWrapper>
+            <p> {name}, </p>
+            <p> priority: {priority}</p>
+            <p>, current status: {status}</p>
+          </ItemWrapper>
+        </label>
+        <br></br>
+      </>
+    );
+  }
 };
 
 export default observer(Item);
--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -12,12 +12,37 @@ export const ItemWrapper = styled.div`
   background-color: #50dfc5;
   width: 500px;
 `;
+
+export const ItemWrapperChecked = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 20px;
+  text-decoration: line-through;
+  margin: 10px;
+  border-style: solid;
+  border-width: 2px;
+  border-color: white;
+  border-radius: 5px;
+  background-color: #50dfc5;
+  width: 500px;
+`;
 export const CreateButtonStyled = styled.button`
   color: white;
   background-color: gray;
   &:hover {
     color: black;
     background-color: lime;
+  }
+`;
+
+export const CheckedButtonStyled = styled.button`
+  color: white;
+  background-color: white;
+  border-style: solid;
+  border-radius: 20px;
+  border-color: black;
+  &:active {
+    background-color: blue;
   }
 `;
 
