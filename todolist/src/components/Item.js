69759458b1878744a9f4dc14dd244f769c170c1a
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-11-23 19:17:54 +0300

    finished setup

---
+++ todolist/src/components/Item.js
@@ -0,0 +1,11 @@
+const Item = () => {
+  return (
+    <div>
+      <p>Study</p>
+      <p>Done</p>
+      <p>Low</p>
+    </div>
+  );
+};
+
+export default Item;
---
+++ todolist/src/components/List.js
@@ -0,0 +1,10 @@
+import Item from "./Item";
+const List = () => {
+  return (
+    <div>
+      <Item></Item>
+    </div>
+  );
+};
+
+export default List;
