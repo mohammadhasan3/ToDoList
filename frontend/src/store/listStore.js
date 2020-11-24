77a58ab3feb5605180f599276ba504b1ac0c66ad
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-11-24 17:16:06 +0300

    Linked data from backend to frontend

--- frontend/src/store/listStore.js
+++ frontend/src/store/listStore.js
@@ -1,19 +1,24 @@
-//import axios from "axios";
 import { makeObservable, observable, action } from "mobx";
+
+import axios from "axios";
 //import slugify from "react-slugify";
 
-import list from "../listdata";
-
 class ListStore {
-  list = list;
+  list = [];
 
   constructor() {
     makeObservable(this, {
       list: observable,
       createTask: action,
       deleteTask: action,
+      fetchList: action,
     });
   }
+
+  fetchList = async () => {
+    const response = await axios.get("http://localhost:8000/list");
+    this.list = response.data;
+  };
   createTask = (newTask) => {
     newTask.id = this.list[this.list.length - 1].id + 1;
 
@@ -26,5 +31,5 @@ class ListStore {
 }
 
 const listStore = new ListStore();
-
+listStore.fetchList();
 export default listStore;
